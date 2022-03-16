fcc68e9a8aa01ddf1dec4c48c35c5d43978dac8d
O-tega <tega.myview@gmail.com>
2022-03-16 02:45:40 +0100

    tailwind installation and configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import {react} from 'react'
+// import {react-router-dom} from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const New =()=>{
+  return <h1 className='text-blue-500 text-center text-2xl'>New project with tailwind</h1>
 }
 
-export default App;
+export default New;
